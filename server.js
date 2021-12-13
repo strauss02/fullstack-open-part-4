5854eedad4113cb56a9f1fdeec88240b96a052df
Ido Strauss <strauss02@hotmail.co.il>
2021-12-13 17:13:41 +0200

    add test folder

--- server.js
+++ server.js
@@ -3,16 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 require('dotenv').config();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 const mongoUrl = process.env.MONGO_URI;
 mongoose
